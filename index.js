51b2142138db8c3cfe62f8eb6d2ab84189669f45
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-15 09:36:15 +0530

    update

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ const port = 8080;
 const path = require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+let posts = [
+    {
+        username : "Nishant kumar",
+        content : "Hello world"
+    },
+    {
+        username : "Prashant kumar",
+        content : "Everything is good"
+    },
+    {
+        username : "Aksahra",
+        content : "Hardwork.."
+    },
+]
 
 app.get("/",(req,res)=>{
     res.send("Welcome");
     console.log("Requesting");
 })
-
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
 app.get("/:Home",(req,res)=>{
     res.send("Welcome to Home")
 })
