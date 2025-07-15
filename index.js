b323f0fc09dc7f6324fc9efc64c1ea1c9e4a5546
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-15 08:36:08 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.set(express.static(path.join(__dirname,"public")));
 
+app.get("/",(req,res)=>{
+    res.send("Welcome");
+    console.log("Requesting");
+})
+
+app.get("/:Home",(req,res)=>{
+    res.send("Welcome to Home")
+})
 app.listen(port,()=>{
     console.log("listening to port 8080");
 });
